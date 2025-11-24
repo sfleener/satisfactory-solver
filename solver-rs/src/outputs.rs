2b1491aef9ba34e1e0487628ea83d19224bce8c8
Scott Fleener <me@scottfleener.com>
2025-11-24 13:47:02 -0500

    Add output ranking

--- solver-rs/src/outputs.rs
+++ solver-rs/src/outputs.rs
@@ -4,7 +4,9 @@ use crate::rational::{ItemsPerMinute, Rat};
 use crate::solver::SolutionValues;
 use petgraph::dot::Dot;
 use petgraph::graph::DiGraph;
-use std::collections::{BTreeMap, HashMap};
+use petgraph::visit::EdgeRef;
+use petgraph::{Direction, EdgeDirection};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 
 pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
     let mut graph = DiGraph::<(Rat<Recipes>, String), ItemsPerMinute>::new();
@@ -19,30 +21,37 @@ pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
         recipe_nodes.insert(recipe_key.clone(), (node, recipe.clone(), recipe_val));
     }
 
+    let mut needs = VecDeque::new();
+    for (k, v) in &settings.outputs {
+        needs.push_back((k.clone(), (output, Rat::from(*v))));
+    }
+
     let mut provides_recipes: BTreeMap<_, BTreeMap<_, _>> = BTreeMap::new();
     for (k, (_, _, v)) in &recipe_nodes {
         let recipe = &data.recipes[k];
         for product in &recipe.products {
             let amount_per_recipe = product.amount;
             let amount = amount_per_recipe * (*v);
-            println!("Adding {k} with {amount:?}: {}", product.item);
+            println!("Adding {k} provides {amount:?} {}", product.item);
             provides_recipes
                 .entry(product.item)
                 .or_default()
                 .insert(*k, (amount_per_recipe, amount));
         }
-    }
 
-    let mut needs = BTreeMap::new();
-    for (k, v) in &settings.outputs {
-        needs.insert(k.clone(), (output, Rat::from(*v)));
+        for ingredient in &recipe.ingredients {
+            let amount_per_recipe = ingredient.amount;
+            let amount = amount_per_recipe * *v;
+            println!("Adding {k} needs {amount:?} {}", ingredient.item);
+            needs.push_back((ingredient.item, (recipe_nodes[k].0, amount)));
+        }
     }
 
     println!("{needs:?}");
 
     let mut needs_resources = BTreeMap::new();
 
-    while let Some((needs_key, (needs_node, mut needs_amount))) = needs.pop_first() {
+    while let Some((needs_key, (needs_node, mut needs_amount))) = needs.pop_front() {
         if data.resources.contains_key(&needs_key) {
             needs_resources.insert(needs_key, (needs_node, needs_amount));
             continue;
@@ -52,7 +61,8 @@ pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
         assert!(!needs_amount.is_zero());
         let Some(provides) = provides_recipes.get_mut(&needs_key) else {
             panic!(
-                "No recipes remaining that provide {needs_amount:?} {needs_key}: {provides_recipes:?}"
+                "No recipes remaining that provide {needs_amount:?} {needs_key}: {provides_recipes:?}\n{:?}",
+                Dot::new(&graph)
             );
         };
         assert!(!provides.is_empty());
@@ -87,18 +97,18 @@ pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
                 to_remove.push(*provides_key);
             }
 
-            let needed_provides_recipes = difference / *provides_amount_per_recipe;
-
-            for ingredient in &provides_recipe.ingredients {
-                let ingredient_per_recipe = ingredient.amount;
-                needs.insert(
-                    ingredient.item,
-                    (
-                        *provides_node,
-                        ingredient_per_recipe * needed_provides_recipes,
-                    ),
-                );
-            }
+            // let needed_provides_recipes = difference / *provides_amount_per_recipe;
+            //
+            // for ingredient in &provides_recipe.ingredients {
+            //     let ingredient_per_recipe = ingredient.amount;
+            //     needs.push_back((
+            //         ingredient.item,
+            //         (
+            //             *provides_node,
+            //             ingredient_per_recipe * needed_provides_recipes,
+            //         ),
+            //     ));
+            // }
 
             if needs_amount.is_zero() {
                 break;
@@ -111,7 +121,7 @@ pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
             provides_recipes.remove(&needs_key);
         }
         if !needs_amount.is_zero() {
-            needs.insert(needs_key, (needs_node, needs_amount.reduced()));
+            needs.push_back((needs_key, (needs_node, needs_amount.reduced())));
         }
     }
 
@@ -124,6 +134,36 @@ pub fn output_graph(settings: &Settings, data: &Data, values: &SolutionValues) {
         });
         graph.add_edge(resource_node, needs_node, needs_amount);
     }
+
+    // assert!(provides_recipes.is_empty(), "{provides_recipes:?}");
+    if !provides_recipes.is_empty() {
+        for (k, v) in provides_recipes {
+            let extra: ItemsPerMinute = v.values().into_iter().map(|(_, a)| *a).sum();
+            let name = &data.items[&k].name;
+            println!("Extra {name}: {extra:?}");
+        }
+    }
+
+    let mut ranks = HashMap::new();
+    let components = petgraph::algo::tarjan_scc(&graph);
+    for node in components.into_iter().flatten() {
+        if ranks.is_empty() {
+            assert_eq!(node, output);
+            ranks.insert(node, 0u16);
+            continue;
+        }
+        let Some(max) = graph
+            .edges_directed(node, Direction::Outgoing)
+            .map(|edge| ranks.get(&edge.target()).copied().unwrap())
+            .max()
+        else {
+            assert_eq!(node, output);
+            ranks.insert(node, 0);
+            continue;
+        };
+        ranks.insert(node, max + 1);
+    }
+    println!("{:?}", ranks);
 
     println!("{:?}", Dot::new(&graph));
 }
